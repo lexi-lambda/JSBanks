9c050e0730264874b4a68bf741e0bbee5adac229
Jake King <imjake9@gmail.com>
2012-04-22 16:52:04 -0700

    Fixed errors when using storage minecrarts.

--- com/imjake9/server/banks/utils/JSBankChestInventory.java
+++ com/imjake9/server/banks/utils/JSBankChestInventory.java
@@ -13,6 +13,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 public class JSBankChestInventory {
@@ -73,6 +74,8 @@ public class JSBankChestInventory {
     
     public static JSBankChestInventory fromInventoryEvent(final InventoryEvent event) {
         if (event.getInventory().getType() != InventoryType.CHEST) return null;
+        InventoryHolder holder = event.getInventory().getHolder();
+        if (!(holder instanceof Chest) || !(holder instanceof DoubleChest)) return null;
         Chest chest = (event.getInventory().getHolder() instanceof DoubleChest)
                 ? (Chest) ((DoubleChest)event.getInventory().getHolder()).getLeftSide()
                 : (Chest) event.getInventory().getHolder();
